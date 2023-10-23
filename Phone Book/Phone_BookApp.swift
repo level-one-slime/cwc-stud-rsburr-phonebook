408e74d69557e80a5e1299560f6b4fed44f3b7ce
Shelton Brown <96554952+rsburr@users.noreply.github.com>
2023-10-23 13:08:21 -0500

    Initial Commit

---
+++ Phone Book/Phone_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phone_BookApp.swift
+//  Phone Book
+//
+//  Created by Shelton Brown on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Phone_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
